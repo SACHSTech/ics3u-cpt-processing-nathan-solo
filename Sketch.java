917dc3dd0e2d526a2cf97be4cc7d750a07c527bf
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2022-01-18 15:29:07 -0500

    Added a function for the toys

--- Sketch.java
+++ Sketch.java
@@ -2,10 +2,31 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Sketch extends PApplet {
-	
+
+  //Note: 4 number in fill or stroke is opacity 
   public float textX = 250;
   public float textY = 350;
-  PImage img;
+  
+  PImage Background;
+
+  /*
+  scene -1 = background with open safe (background -1)
+  scene 0 = background with no painting (background 0)
+  scene 1 = original background (background 1)
+  scene 2 = toys
+  scene 3 = page
+  scene 4 = page 2  
+  scene 4 = window
+  scene 5 = door
+  scene 6 = bed
+  scene 7 = text
+  */
+
+  public float scene = 1;
+  public float background = 1;
+  
+  // to make sure the player does not guess the code. 
+  public float step;
 
   public void settings() {
     size(1500, 1000);
@@ -14,27 +35,62 @@ public class Sketch extends PApplet {
 
   public void setup() {
     background(210, 255, 173);
-    img = loadImage("Abandoned.Asylum.jpg");
+    Background = loadImage("Abandoned.Asylum.jpg");
 
   }
 
   public void draw() {
 	  
+    if (scene == 1 && background == 1) {
+      image(Background, 0, 0);
 
-    image(img, 0, 0);
-    textbox();
+      if (mousePressed){
 
-  }
+        if (mouseX >= 206 && mouseX <= 366 && mouseY >= 690 && mouseY <= 840){
+          scene = 2;
+        
+        }
+      }
+
+    } else if (scene == 2) {
+        image(Background, 0, 0);
+        
+        stroke(0);
+        fill(0, 0, 0, 210);
+        rect(0, 0, 1500, 1000);
+      }
+
+
+
+
+
+
+
+
+
+
+
+}
 
   public void textbox() {
-    stroke(0);
-    fill(0);
+    stroke(255);
+    fill(88, 85, 90);
 
-    rect(200, 300, 600, 300);
+    rect(200, 300, 1100, 400);
 
     stroke(255);
     fill(255, 0, 0);
-    //ellipse()
+
+    ellipse(1250, 350, 50, 50);
+    
+    line(1240, 340, 1260, 360);
+    line(1260, 340, 1240, 360);
+
+    if (mousePressed){
+      if (mouseX > 1200 && mouseX < 1300 && mouseY > 300 && mouseY < 400){
+
+      }
+    }
   }
   
   // define other methods down here.
